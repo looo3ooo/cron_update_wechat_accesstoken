c72ef0fbcda0ffd36c4d30bffe67fcb7d2f5fe98
chao <924447294@qq.com>
2018-01-05 15:48:11 +0800

    first commit

--- tools/accessControlAllowOrigin.go
+++
@@ -1,9 +0,0 @@
-package tools
-
-import "net/http"
-
-func AccessControlAllowOrigin(w http.ResponseWriter)  {
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	//w.Header().Add("Access-Control-Allow-Headers", "Content-Type") //header的类型
-	//w.Header().Set("content-type", "application/json")             //返回数据格式是json
-}
